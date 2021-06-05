a37c75a9bdb362a6fed490a796160507c2ce844d
CodeTheDev <code@codeslab.ca>
2021-06-05 00:19:26 -0400

    Created Bug command - Created basic outline for Bug command.

---
+++ src/main/java/dev/codeerror/sootreports/command/Bug.java
@@ -0,0 +1,26 @@
+package dev.codeerror.sootreports.command;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class Bug extends Command {
+
+    public Bug() { super("Bug"); }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (sender instanceof ProxiedPlayer) {
+
+
+
+        } else {
+
+
+
+        }
+
+    }
+
+}
