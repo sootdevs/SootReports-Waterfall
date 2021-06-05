4be469a067b80d08d9399fd20a107a03c80da0a0
CodeTheDev <code@codeslab.ca>
2021-06-05 00:19:41 -0400

    Created Suggest command - Created basic outline for Suggest command.

---
+++ src/main/java/dev/codeerror/sootreports/command/Suggest.java
@@ -0,0 +1,26 @@
+package dev.codeerror.sootreports.command;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class Suggest extends Command {
+
+    public Suggest() { super("Suggest"); }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (sender instanceof ProxiedPlayer) {
+
+
+
+        } else {
+
+
+
+        }
+
+    }
+
+}
