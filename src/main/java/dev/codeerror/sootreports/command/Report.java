bde66b1d7d0162f0f09734c545f0026d41e7a0cd
CodeTheDev <code@codeslab.ca>
2021-06-05 00:19:14 -0400

    Created Report command - Created basic outline for Report command.

---
+++ src/main/java/dev/codeerror/sootreports/command/Report.java
@@ -0,0 +1,26 @@
+package dev.codeerror.sootreports.command;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class Report extends Command {
+
+    public Report() { super("Report"); }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (sender instanceof ProxiedPlayer) {
+
+
+
+        } else {
+
+
+
+        }
+
+    }
+
+}
