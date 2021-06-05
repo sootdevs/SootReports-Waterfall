5a4567f704b2ccd8183a9bd0c065454bf4dfe00e
CodeTheDev <code@codeslab.ca>
2021-06-04 21:59:51 -0400

    Created SootReports class - Main class for plugin. - Will contain most of plugin programming.

---
+++ src/main/java/dev/codeerror/sootreports/SootReports.java
@@ -0,0 +1,21 @@
+package dev.codeerror.sootreports;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class SootReports extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+
+}
