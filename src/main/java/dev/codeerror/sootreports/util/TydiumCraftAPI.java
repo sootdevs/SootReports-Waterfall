49febefffbbde33b667c60f04db0e5e18cb38e82
CodeTheDev <code@codeslab.ca>
2021-06-10 01:08:52 -0400

    Updated TydiumCraftAPI - Completely implemented all features of skin API.

--- src/main/java/dev/codeerror/sootreports/util/TydiumCraftAPI.java
+++ src/main/java/dev/codeerror/sootreports/util/TydiumCraftAPI.java
@@ -8,9 +8,38 @@ public class TydiumCraftAPI {
 
     public String getSkinURL(String type, String size, String flip) {
 
-        String skin = "https://api.tydiumcraft.net/skin?uuid=" + this.uuid;
+        String baseURL = "https://api.tydiumcraft.net/skin?uuid=" + this.uuid;
+        StringBuilder urlBuilder = new StringBuilder(baseURL);
 
-        return skin;
+        if (type == null) {
+
+            urlBuilder.append("&type=body");
+            return new String(urlBuilder);
+
+        } else if (type.equals("skin")) {
+
+            urlBuilder.append("&type=skin&download");
+            return new String(urlBuilder);
+
+        }
+
+        urlBuilder.append("&type=").append(type);
+
+        if (size != null) {
+
+            urlBuilder.append("&size=").append(Integer.parseInt(size));
+            return new String(urlBuilder);
+
+        }
+
+        if ((type.equals("body") || type.equals("head")) && (flip.equals("left") || flip.equals("right"))) {
+
+            urlBuilder.append("&direction=").append(flip);
+            return new String(urlBuilder);
+
+        }
+
+        return new String(urlBuilder);
 
     }
 
