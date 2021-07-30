13aae896da9b9c1380812761b9d4798e74dd974d
CodeTheDev <code@codeslab.ca>
2021-07-30 01:10:58 -0400

    Updated SootReports class - Completely implemented configuration. - Created chatPrefix variable.

--- src/main/java/dev/codeerror/sootreports/SootReports.java
+++ src/main/java/dev/codeerror/sootreports/SootReports.java
@@ -1,36 +1,54 @@
 package dev.codeerror.sootreports;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import dev.codeerror.sootreports.command.Bug;
+import dev.codeerror.sootreports.command.Report;
+import dev.codeerror.sootreports.command.Suggest;
+import dev.codeerror.sootreports.util.ConfigurationUtils;
+import dev.codeerror.sootreports.util.DiscordWebhook;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.TabExecutor;
 import net.md_5.bungee.config.Configuration;
 
-import dev.codeerror.sootreports.command.Bug;
-import dev.codeerror.sootreports.util.DiscordWebhook;
-import dev.codeerror.sootreports.command.Report;
-import dev.codeerror.sootreports.command.Suggest;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SootReports extends Plugin implements TabExecutor {
 
-    private final Configuration config = new Configuration();
-    private final DiscordWebhook webhook = new DiscordWebhook(config.getString("discord-webhook-url"));
+    private static SootReports instance;
+
+    private final ConfigurationUtils configUtils = new ConfigurationUtils("config.yml");
+    private final Configuration config = configUtils.getConfig();
+    private final DiscordWebhook webhook = new DiscordWebhook(config.getString("discord-webhook"));
+    private final TextComponent chatPrefix = buildChatPrefix();
 
     @Override
     public void onEnable() {
 
-        this.getLogger().info("Setting up Discord Webhook...");
+        // Define Instance
+        instance = this;
+
+        // Configuration
+        configUtils.saveDefaultConfig();
+
+        // Commands
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Report());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Bug());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Suggest());
+        this.getLogger().info("Successfully registered commands!");
+
+        // Discord Webhook
         webhook.setAvatarUrl("https://cloud.sootmc.com/images/sootreportswebhook.png");
         webhook.setUsername("SootReports");
         this.getLogger().info("Successfully set up Discord Webhook!");
 
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Report());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Bug());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Suggest());
+        // Completion
+        this.getLogger().info("SootReports v1.0 has been successfully enabled! Created by CodeError.");
 
     }
 
@@ -65,6 +83,18 @@ public class SootReports extends Plugin implements TabExecutor {
 
     }
 
+    public static SootReports getInstance() { return instance; }
+
     public DiscordWebhook getWebhook() { return webhook; }
+    private TextComponent buildChatPrefix() {
+
+        ComponentBuilder builder = new ComponentBuilder();
+        builder.append("[").color(ChatColor.DARK_GRAY);
+        builder.append("SootReports").color(ChatColor.BLUE);
+        builder.append("] ").color(ChatColor.DARK_GRAY);
+        return new TextComponent(builder.create());
+
+    }
+    public TextComponent getChatPrefix() { return chatPrefix; }
 
 }
