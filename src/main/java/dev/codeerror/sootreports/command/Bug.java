c9643e6d19f6aca5dd1406e0cfdde6a3e0ea769e
CodeTheDev <code@codeslab.ca>
2021-08-01 18:03:23 -0400

    Completed bug command - Completely created/converted bug command from Velocity version.

--- src/main/java/dev/codeerror/sootreports/command/Bug.java
+++ src/main/java/dev/codeerror/sootreports/command/Bug.java
@@ -1,25 +1,108 @@
 package dev.codeerror.sootreports.command;
 
+import dev.codeerror.sootreports.SootReports;
+import dev.codeerror.sootreports.util.DiscordWebhook;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
+import java.awt.*;
+
 public class Bug extends Command {
+
+    private final TextComponent chatPrefix = SootReports.getInstance().getChatPrefix();
 
     public Bug() { super("bug"); }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
 
+        String bug = args[0];
+
+        if (bug.isEmpty()) {
+
+            if (sender instanceof ProxiedPlayer) {
+
+                ComponentBuilder error = new ComponentBuilder();
+                error.append("Too few arguments! Correct Syntax: ").color(ChatColor.RED);
+                error.append("/bug <bug>").color(ChatColor.YELLOW);
+
+                sender.sendMessage(chatPrefix, new TextComponent(error.create()));
+
+            }
+
+            return;
+
+        }
+
         if (sender instanceof ProxiedPlayer) {
 
+            TextComponent success = new TextComponent();
+            success.setText("Bug report has been successfully created and sent to the developer team. Thank you for contributing to the efforts of the developer team.");
+            success.setColor(ChatColor.GREEN);
 
+            webhookBug(bug, sender);
+            SootReports.getInstance().getLogger().info(sender.getName() + " created a new bug report.");
+            sender.sendMessage(chatPrefix, success);
 
         } else {
 
-
+            webhookBug(bug, sender);
+            SootReports.getInstance().getLogger().info("CONSOLE created a new bug report.");
 
         }
+
+    }
+
+    @Override
+    public boolean hasPermission(CommandSender sender) { return sender.hasPermission("sootreports.command.bug"); }
+
+    private void webhookBug(String bug, CommandSender sender) {
+
+        DiscordWebhook.EmbedObject embed = new DiscordWebhook.EmbedObject();
+
+        embed.setColor(Color.YELLOW);
+        embed.setTitle("Bug Report");
+
+        if (sender instanceof ProxiedPlayer) {
+
+            if (((ProxiedPlayer) sender).getServer().isConnected()) {
+
+                embed.addField("Server", "`" + ((ProxiedPlayer) sender).getServer().getInfo().getName() + "`", false);
+
+            } else {
+
+                embed.addField("Server", "`Null`", false);
+
+            }
+
+            embed.addField("Bug", bug, false);
+            embed.addField("Reporter", "`" + sender.getName() + "`", false);
+
+        } else {
+
+            embed.addField("Server", "`Proxy`", false);
+            embed.addField("Bug", bug, false);
+            embed.addField("Reporter", "`CONSOLE`", false);
+
+        }
+
+        SootReports.getInstance().getWebhook().addEmbed(embed);
+
+        try {
+
+            SootReports.getInstance().getWebhook().execute();
+
+        } catch (Exception e) {
+
+            SootReports.getInstance().getLogger().severe("A bug report has failed to send! Ensure that your configuration is correct!");
+
+        }
+
+        SootReports.getInstance().getWebhook().removeEmbed(embed);
 
     }
 
